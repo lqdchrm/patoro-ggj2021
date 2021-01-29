e9c673c27d24b5a40c7c8d87100bf6986a23b9b9
lqdchrm <lqdchrm-code@yahoo.de>
2021-01-29 20:25:08 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace laf_ggj2021
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            DotNetEnv.Env.Load();
+
+            var discordClient = new DiscordClient(new DiscordConfiguration
+            {
+                Token = System.Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+                TokenType = TokenType.Bot
+            });
+
+            discordClient.MessageCreated += OnMessageCreated;
+
+            await discordClient.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task OnMessageCreated(MessageCreateEventArgs e)
+        {
+            if (string.Equals(e.Message.Content, "hello", StringComparison.OrdinalIgnoreCase))
+            {
+                await e.Message.RespondAsync(e.Message.Author.Username);
+            }
+        }
+    }
+}
