e4ce0b17b4cc97d7a7488e1010562c9f813e3d85
Patrick Kranz <patrick-kranz@live.de>
2021-02-01 23:00:42 +0100

    Add first try parser

---
+++ Game/FindLosty/CommandParser.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LostAndFound.Game.FindLosty
+{
+    public class CommandParser
+    {
+
+        private int index;
+        private readonly object[] objects;
+        private readonly IList<string> commandList;
+
+        public CommandParser(object[] vs, IList<string> commandList)
+        {
+            this.objects = vs;
+            this.commandList = commandList;
+        }
+
+        public class CommandParserBuilder
+        {
+            private List<object> argumentList = new List<object>();
+            private IList<string> commandList;
+
+            public CommandParserBuilder(IList<string> commandList)
+            {
+                this.commandList = commandList;
+            }
+
+            public CommandParser Build()
+            {
+                return new CommandParser(argumentList.ToArray(), this.commandList);
+            }
+
+            public CommandParserBuilder For(CommonRoom room, bool includeItems = true)
+            {
+                argumentList.AddRange(room.KnownThings);
+                if (includeItems)
+                    argumentList.AddRange(room.Inventory.Values);
+                return this;
+            }
+            public CommandParserBuilder For(Inventory inventory)
+            {
+                argumentList.AddRange(inventory.Values);
+                return this;
+            }
+
+            /// <summary>
+            /// Will add parsing for this Players Name
+            /// </summary>
+            /// <param name="player"></param>
+            /// <returns></returns>
+            public CommandParserBuilder For(Player player)
+            {
+                argumentList.Add(player);
+                return this;
+            }
+
+            public CommandParserBuilder For(IEnumerable<Player> players)
+            {
+                foreach (var p in players)
+                {
+                    this.For(p);
+                }
+                return this;
+            }
+            public CommandParserBuilder For(params Player[] players)
+            {
+                return For(players as IEnumerable<Player>);
+            }
+
+
+
+        }
+
+        public Player TakePlayer()
+        {
+            return objects.OfType<Player>().Where(x => TryTake(x.Name)).FirstOrDefault();
+        }
+        public Item TakeItem()
+        {
+            return objects.OfType<Item>().Where(x => TryTake(x.Name)).FirstOrDefault();
+        }
+        public string TakeString(params string[] strs)
+        {
+            return strs.Where(x => TryTake(x.AsSpan())).FirstOrDefault();
+        }
+
+        private bool TryTake(ReadOnlySpan<char> name)
+        {
+            var splittedName = name.Split(' ');
+            var currentIndex = index;
+            foreach (var part in splittedName)
+            {
+                if (index >= commandList.Count)
+                    return false;
+
+                if (commandList[index] != name[part])
+                    return false;
+                currentIndex++;
+            }
+            this.index = currentIndex;
+            return true;
+        }
+    }
+}
--- Game/FindLosty/Utils.cs
+++ Game/FindLosty/Utils.cs
@@ -60,5 +60,96 @@ namespace LostAndFound.Game.FindLosty
 
             return inner?.Mentions.Cast<Player>().Concat(argumentMentioneds).Distinct().ToList() ?? Array.Empty<Player>() as IList<Player>;
         }
+
+        internal static SpanSplitEnumerator<T> Split<T>(this ReadOnlySpan<T> span, T separator)
+            where T : IEquatable<T>
+            => new SpanSplitEnumerator<T>(span, separator);
+        internal static SpanSplitEnumerator<T> Split<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> separator)
+            where T : IEquatable<T>
+            => new SpanSplitEnumerator<T>(span, separator);
+
+        internal static CommandParser.CommandParserBuilder Parser(this IList<string> list)
+        {
+            return new CommandParser.CommandParserBuilder(list);
+        }
+
+    }
+
+    internal ref struct SpanSplitEnumerator<T> where T : IEquatable<T>
+    {
+        private readonly ReadOnlySpan<T> _buffer;
+
+        private readonly ReadOnlySpan<T> _separators;
+        private readonly T _separator;
+
+        private readonly int _separatorLength;
+        private readonly bool _splitOnSingleToken;
+
+        private readonly bool _isInitialized;
+
+        private int _startCurrent;
+        private int _endCurrent;
+        private int _startNext;
+
+        /// <summary>
+        /// Returns an enumerator that allows for iteration over the split span.
+        /// </summary>
+        /// <returns>Returns a <see cref="System.SpanSplitEnumerator{T}"/> that can be used to iterate over the split span.</returns>
+        public SpanSplitEnumerator<T> GetEnumerator() => this;
+
+        /// <summary>
+        /// Returns the current element of the enumeration.
+        /// </summary>
+        /// <returns>Returns a <see cref="System.Range"/> instance that indicates the bounds of the current element withing the source span.</returns>
+        public Range Current => new Range(_startCurrent, _endCurrent);
+
+        internal SpanSplitEnumerator(ReadOnlySpan<T> span, ReadOnlySpan<T> separators)
+        {
+            _isInitialized = true;
+            _buffer = span;
+            _separators = separators;
+            _separator = default!;
+            _splitOnSingleToken = false;
+            _separatorLength = _separators.Length != 0 ? _separators.Length : 1;
+            _startCurrent = 0;
+            _endCurrent = 0;
+            _startNext = 0;
+        }
+
+        internal SpanSplitEnumerator(ReadOnlySpan<T> span, T separator)
+        {
+            _isInitialized = true;
+            _buffer = span;
+            _separator = separator;
+            _separators = default;
+            _splitOnSingleToken = true;
+            _separatorLength = 1;
+            _startCurrent = 0;
+            _endCurrent = 0;
+            _startNext = 0;
+        }
+
+
+        /// <summary>
+        /// Advances the enumerator to the next element of the enumeration.
+        /// </summary>
+        /// <returns><see langword="true"/> if the enumerator was successfully advanced to the next element; <see langword="false"/> if the enumerator has passed the end of the enumeration.</returns>
+        public bool MoveNext()
+        {
+            if (!_isInitialized || _startNext > _buffer.Length)
+            {
+                return false;
+            }
+
+            ReadOnlySpan<T> slice = _buffer.Slice(_startNext);
+            _startCurrent = _startNext;
+
+            int separatorIndex = _splitOnSingleToken ? slice.IndexOf(_separator) : slice.IndexOf(_separators);
+            int elementLength = (separatorIndex != -1 ? separatorIndex : slice.Length);
+
+            _endCurrent = _startCurrent + elementLength;
+            _startNext = _endCurrent + _separatorLength;
+            return true;
+        }
     }
 }
