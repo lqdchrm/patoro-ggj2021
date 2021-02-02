d9ea0d25cbed9b7d83a528bf83ba64f66af4c136
lqdchrm <lqdchrm-code@yahoo.de>
2021-02-02 14:03:41 +0100

    added auto-appear of items when mentioned

--- FindLosty/TypeDefs.cs
+++ FindLosty/TypeDefs.cs
@@ -7,9 +7,24 @@ using System.Threading.Tasks;
 
 namespace LostAndFound.FindLosty
 {
-    public abstract class Room : BaseRoom<FindLostyGame, Room, Player, Thing> { public Room(FindLostyGame game, string name = null) : base(game, name) { } }
-    public abstract class Thing : BaseThing<FindLostyGame, Room, Player, Thing> { public Thing(FindLostyGame game, string name = null) : base(game, false, name) { } }
-    public abstract class Item : BaseThing<FindLostyGame, Room, Player, Thing> { public Item(FindLostyGame game, string name = null) : base(game, true, name) { } }
-    public abstract class Container : BaseContainer<FindLostyGame, Room, Player, Thing> { public Container(FindLostyGame game, string name = null) : base(game, true, false, name) { } }
+    public abstract class Room : BaseRoom<FindLostyGame, Room, Player, Thing> {
+        public Room(FindLostyGame game) : this(game, null) { }
+        public Room(FindLostyGame game, string name) : base(game, name) { }
+    }
+
+    public abstract class Thing : BaseThing<FindLostyGame, Room, Player, Thing> {
+        public Thing(FindLostyGame game) : this(game, null) { }
+        public Thing(FindLostyGame game, string name) : base(game, false, name) { }
+    }
+
+    public abstract class Item : BaseThing<FindLostyGame, Room, Player, Thing> {
+        public Item(FindLostyGame game) : this(game, null) { }
+        public Item(FindLostyGame game, string name) : base(game, true, name) { }
+    }
+    public abstract class Container : BaseContainer<FindLostyGame, Room, Player, Thing>
+    {
+        public Container(FindLostyGame game) : this(game, null) { }
+        public Container(FindLostyGame game, string name) : base(game, true, false, name) { }
+    }
 
 }
