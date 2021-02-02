14bc81c2d8e5bbd4e4be917fec246bb0a6300d82
Tobias Krumholz <krumholt@gmail.com>
2021-02-02 20:03:12 +0100

    kitchen, shelves

---
+++ FindLosty/03_Kitchen/Shelves.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostAndFound.Game.FindLosty.Things
+{
+    public class Shelves : Container
+    {
+
+        bool DoorsOpen = false;
+        
+        public Shelves(FindLostyGame game) : base(game)
+        {
+        }
+
+        public override string LookText(Thing other)
+        {
+            string message = "";
+            if (other == null)
+                return $"Some shelves.";
+            return $"This is the best shelf ever";
+        }
+
+        public override string UseText(Thing other)
+        {
+            return $"This is the best shelf ever";
+        }
+
+        public override bool Use(Player player, Thing other)
+        {
+            var txt = UseText(other);
+
+            player.SendTextWithState(txt);
+            return true;
+        }
+    }
+}
