0f284e3f6dab5bdbdb032216e2ce23aa682a1f8d
Patrick Kranz <patrick-kranz@live.de>
2021-02-03 00:26:00 +0100

    implemented use in staircases

--- FindLosty/01_EntryHall/Staircase.cs
+++ FindLosty/01_EntryHall/Staircase.cs
@@ -23,6 +23,8 @@ namespace LostAndFound.FindLosty._01_EntryHall
          ╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚══════╝
          */
 
+        private bool isBroken;
+
         /*
         ██╗      ██████╗  ██████╗ ██╗  ██╗
         ██║     ██╔═══██╗██╔═══██╗██║ ██╔╝
@@ -31,7 +33,12 @@ namespace LostAndFound.FindLosty._01_EntryHall
         ███████╗╚██████╔╝╚██████╔╝██║  ██╗
         ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝
         */
-        public override string LookText => @$"
+        public override string LookText => isBroken
+            ? @$"
+                Like many elements in this room the stairs are made of a dark wood.
+                But the lower stairs are broken... you can no longer use this.
+                ".FormatMultiline()
+            : @$"
             Like many elements in this room the stairs are made of a dark wood.
             But it looks old and ... not in a good way. It could be a hazard.
             In the side of the {this} is a {Game.EntryHall.MetalDoor}."
@@ -100,6 +107,37 @@ namespace LostAndFound.FindLosty._01_EntryHall
         ╚██████╔╝███████║███████╗
          ╚═════╝ ╚══════╝╚══════╝
         */
+
+        public override bool Use(Player sender, BaseThing<FindLostyGame, Room, Player, Thing> other, bool isFlippedCall = false)
+        {
+            if (other is not null)
+                return base.Use(sender, other, isFlippedCall);
+            if (isBroken)
+            {
+                sender.Reply("There is not enogh left of the stairs to climb up.");
+            }
+            else
+            {
+                sender.Reply(@"
+You go up the stairs, but half way the stairs break and with a louad squash you fell down.
+You recive Damage.
+".FormatMultiline());
+                sender.Room.SendText(@$"You see how {sender} climbs up the stairs, when suddenly the staircase collapses and {sender} forcfully hits the ground.", sender);
+                Game.DiningRoom.SendText(@$"
+You hear a load noise. Something seems to be collapsed in the {Game.EntryHall}.
+");
+                Game.LivingRoom.SendText(@$"
+You hear a load noise. Something seems to be collapsed in the {Game.EntryHall}.
+");
+                Game.Kitchen.SendText(@$"
+You hear a load noise. Something seems to be collapsed in another room.
+");
+                sender.Hit();
+                isBroken = true;
+            }
+
+            return false;
+        }
 
         /*
         ██╗  ██╗███████╗██╗     ██████╗ ███████╗██████╗ ███████╗
